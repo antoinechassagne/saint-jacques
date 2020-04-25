5464ff4fe409c159fdb14e74e426e22495f4e883
Antoine Chassagne <antoine.chassagne@me.com>
2020-04-25 16:49:51 +0200

    Add Spot route in settings and update the way we render menu

--- client/src/settings/routes.js
+++ client/src/settings/routes.js
@@ -1,24 +1,35 @@
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Spots from "../pages/Spots";
+import Spot from "../pages/Spot";
 
 export default {
   home: {
     path: "/",
     label: "Accueil",
     component: Home,
+    menu: true,
     guard: false,
   },
   spots: {
     path: "/spots",
     label: "Spots",
     component: Spots,
-    guard: true,
+    menu: true,
+    guard: false,
+  },
+  spot: {
+    path: "/spots/:id",
+    label: "Spot",
+    component: Spot,
+    menu: false,
+    guard: false,
   },
   login: {
     path: "/login",
     label: "Se connecter",
     component: Login,
+    menu: true,
     guard: false,
   },
 };
