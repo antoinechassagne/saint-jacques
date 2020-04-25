68b9dbd36ab478a3d91208b967511ef05d0f22b8
Antoine Chassagne <antoine.chassagne@me.com>
2020-04-26 00:07:16 +0200

    Add register route

---
+++ src/Controller/Api/SignupController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class SignupController extends AbstractController
+{
+    /**
+     * @Route("/signup", name="signup")
+     */
+    public function signup(Request $request, UserPasswordEncoderInterface $passwordEncoder): JsonResponse
+    {
+        if ($request->getMethod() === "POST") {
+            $body = json_decode($request->getContent());
+            
+            $user = new User();
+            $user->setEmail($body->email);
+            $user->setPassword($passwordEncoder->encodePassword($user, $body->password));
+
+            try {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $response = new JsonResponse();
+                $response->setStatusCode(JsonResponse::HTTP_CREATED);
+                return $response;
+            } catch (\Exception $e) 
+            {
+                $response = new JsonResponse($e);
+                $response->setStatusCode(JsonResponse::HTTP_BAD_REQUEST);
+                return $response;
+            }
+        }
+    }
+}
