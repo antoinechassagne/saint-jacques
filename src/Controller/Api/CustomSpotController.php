461e384f101903cd04e46a2ec57087f7a965f3c9
Antoine Chassagne <antoine.chassagne@me.com>
2020-05-10 19:09:52 +0200

    Add isFavorite flag on get spot

--- src/Controller/Api/CustomSpotController.php
+++ src/Controller/Api/CustomSpotController.php
@@ -11,6 +11,32 @@ use Symfony\Component\Serializer\SerializerInterface;
 class CustomSpotController extends AbstractController
 {
     /**
+     * @Route("api/spots/{id}", name="get_spot")
+     */
+    public function getSpot($id, Request $request, SerializerInterface $serializer) {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        $spot = $em->getRepository(Spot::class)->find($id);
+        $result = json_decode($serializer->serialize($spot, "json"), JSON_UNESCAPED_SLASHES);
+        
+        // Add flag to response
+        $result['isFavorite'] = false;
+        if ($user) {
+            foreach($result['users'] as $key => $value) {
+                if ((int)basename($value) === $user->getId()) {
+                    $result['isFavorite'] = true;
+                    break;
+                }
+            }
+        }
+        
+        // Remove users from response
+        unset($result['users']);
+
+        return new JsonResponse($result);
+    }
+
+    /**
     * @Route("api/search/spots", name="search_spots")
     */
     public function search(Request $request, SerializerInterface $serializer)
