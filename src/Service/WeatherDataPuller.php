7b68651daa1bb90bc4121de1760387e56100b86e
Antoine Chassagne <antoine.chassagne@me.com>
2020-05-11 00:46:07 +0200

    Add weather data puller service

---
+++ src/Service/WeatherDataPuller.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpClient\HttpClient;
+
+class WeatherDataPuller
+{
+    private $apiUrl;
+    private $apiKey;
+    private $client;
+
+    public function __construct(string $apiUrl, string $apiKey) 
+    {
+        $this->apiUrl = $apiUrl;
+        $this->apiKey = $apiKey;
+        $this->client = HttpClient::create();
+    }
+
+    /**
+     * Pull weather data
+     *
+     * @param float $latitude
+     * @param float $longitude
+     * @return 
+     */
+    public function pull(float $latitude, float $longitude)
+    {
+        $body = [
+            "lat" => $latitude,
+            "lon" => $longitude,
+            "model" => "gfs",
+            "parameters" => ["wind", "dewpoint", "rh", "pressure"],
+            "levels" => ["surface", "800h", "300h"],
+            "key" => $this->apiKey
+        ];
+
+        $response = $this->client->request('POST', $this->apiUrl, ['json' => $body]);
+        $status = $response->getStatusCode();
+        
+        if ($status !== 200) {
+            return null;
+        }
+
+        return json_decode($response->getContent()); 
+    }
+}
