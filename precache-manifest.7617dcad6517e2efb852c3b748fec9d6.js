c5994af004eeb51b12f3c68e4adc1395a174e0bd
Antoine Chassagne <antoine.chassagne@me.com>
2020-05-13 07:54:12 +0200

    Updates

---
+++ precache-manifest.7617dcad6517e2efb852c3b748fec9d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8408f5da5e4ddb473a918c82e8ed0986",
+    "url": "/saint-jacques/index.html"
+  },
+  {
+    "revision": "cca84a687a6141566f11",
+    "url": "/saint-jacques/static/css/main.73183148.chunk.css"
+  },
+  {
+    "revision": "e8bf0ad5928bf42a6c26",
+    "url": "/saint-jacques/static/js/2.630c08c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/saint-jacques/static/js/2.630c08c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca84a687a6141566f11",
+    "url": "/saint-jacques/static/js/main.e6d157a2.chunk.js"
+  },
+  {
+    "revision": "57599aa95d03ce279f79",
+    "url": "/saint-jacques/static/js/runtime-main.92ccb2f1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/saint-jacques/precache-manifest.91ce0e61a96d2e7587022dac33447f87.js"
+  "/saint-jacques/precache-manifest.7617dcad6517e2efb852c3b748fec9d6.js"
 );
 
 self.addEventListener('message', (event) => {
