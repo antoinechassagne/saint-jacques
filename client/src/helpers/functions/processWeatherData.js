629261a07ec3d3413c118844c0da32b81329ea56
Antoine Chassagne <antoine.chassagne@me.com>
2020-05-14 15:40:34 +0200

    Cut data

--- client/src/helpers/functions/processWeatherData.js
+++ client/src/helpers/functions/processWeatherData.js
@@ -6,6 +6,7 @@ const processWeatherData = (data, dataLabel) => {
   if (!data[dataLabel][0]) {
     return false;
   }
+  data.ts = data.ts.splice(0, Math.floor(data.ts.length / 2));
   return data.ts.map((ts, i) => ({
     date: moment(ts),
     waveHeight: parseFloat(data[dataLabel][i].toFixed(2)),
